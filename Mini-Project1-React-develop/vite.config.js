90dc3cd8c43a03a7312d84499aad599ef6d0fff9
DavidSantiago00 <davidsantiagotorresrestrepo@gmail.com>
2025-10-17 08:28:02 -0500

    deploy: trigger Git pages build

--- Mini-Project1-React-develop/vite.config.js
+++ Mini-Project1-React-develop/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mini-Project1-React/', // 👈 importante: debe tener slash inicial y final
+  base: '/carta-que-cambia-de-color/', // 👈 importante: debe tener slash inicial y final
 })
